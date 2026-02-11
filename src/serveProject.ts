f18e6d4c523994e0eda7bf18f4947a724717e59d
Joe <joe.melsha@live.com>
2026-02-11 16:17:56 -0600

    Add ServeTerminal class to manage 'atlas serve' process in a pseudoterminal
    This commit introduces the ServeTerminal class, which utilizes Node.js child processes to run the 'atlas serve' command within a VS Code terminal. It handles process output, errors, and cleanup upon termination, enhancing the project's terminal management capabilities.

--- src/serveProject.ts
+++ src/serveProject.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode"
+import * as cp from "child_process"
 import { State } from "./extension"
 import { ProjectFile } from "./findProjectFiles"
 import { updateButton } from "./updateButton"
@@ -8,6 +9,68 @@ import path = require("path")
 export type RunningProject = {
   stop: () => void
   projectFile: ProjectFile
+}
+
+class ServeTerminal implements vscode.Pseudoterminal {
+  private writeEmitter = new vscode.EventEmitter<string>()
+  private closeEmitter = new vscode.EventEmitter<number | void>()
+
+  onDidWrite = this.writeEmitter.event
+  onDidClose = this.closeEmitter.event
+
+  private process?: cp.ChildProcess
+
+  constructor(
+    private cwd: string,
+    private projectFileName: string,
+    private onProcessExit: () => void,
+  ) {}
+
+  open(): void {
+    this.process = cp.spawn("atlas", ["serve", this.projectFileName], {
+      cwd: this.cwd,
+    })
+
+    this.process.stdout?.on("data", (data: Buffer) => {
+      this.writeEmitter.fire(data.toString().replace(/\r?\n/g, "\r\n"))
+    })
+
+    this.process.stderr?.on("data", (data: Buffer) => {
+      this.writeEmitter.fire(data.toString().replace(/\r?\n/g, "\r\n"))
+    })
+
+    this.process.on("exit", (code) => {
+      this.writeEmitter.fire(
+        `\r\n\x1b[2mProcess exited with code ${code}\x1b[0m\r\n`,
+      )
+      this.onProcessExit()
+    })
+
+    this.process.on("error", (err: NodeJS.ErrnoException) => {
+      if (err.code === "ENOENT") {
+        this.writeEmitter.fire(
+          `\r\n\x1b[31mCould not find 'atlas' on PATH. Is it installed?\x1b[0m\r\n`,
+        )
+      } else {
+        this.writeEmitter.fire(
+          `\r\n\x1b[31mFailed to start process: ${err.message}\x1b[0m\r\n`,
+        )
+      }
+      this.onProcessExit()
+    })
+  }
+
+  close(): void {
+    this.process?.kill()
+  }
+
+  handleInput(data: string): void {
+    if (data === "\x03") {
+      this.process?.kill()
+    } else {
+      this.process?.stdin?.write(data)
+    }
+  }
 }
 
 export function serveProject(state: State, projectFile: ProjectFile) {
@@ -22,18 +85,29 @@ export function serveProject(state: State, projectFile: ProjectFile) {
   const projectFileFolder = path.dirname(projectFilePath)
   const projectFileName = path.basename(projectFilePath)
 
-  const terminal = vscode.window.createTerminal({
-    name: `Atlas: ${formatProjectDisplayName(projectFile)}`,
-    cwd: projectFileFolder,
-  })
-  terminal.show()
-  terminal.sendText(`atlas serve "${projectFileName}"`)
-
-  // Track the terminal so we can stop it
-  const disposeListener = vscode.window.onDidCloseTerminal((closedTerminal) => {
-    if (closedTerminal === terminal) {
+  const cleanupProject = () => {
+    if (projectFilePath in state.running) {
       delete state.running[projectFilePath]
       updateButton(state)
+    }
+  }
+
+  const pty = new ServeTerminal(
+    projectFileFolder,
+    projectFileName,
+    cleanupProject,
+  )
+
+  const terminal = vscode.window.createTerminal({
+    name: `Atlas: ${formatProjectDisplayName(projectFile)}`,
+    pty,
+  })
+  terminal.show()
+
+  // Also clean up when the terminal UI is closed
+  const disposeListener = vscode.window.onDidCloseTerminal((closedTerminal) => {
+    if (closedTerminal === terminal) {
+      cleanupProject()
       disposeListener.dispose()
     }
   })
